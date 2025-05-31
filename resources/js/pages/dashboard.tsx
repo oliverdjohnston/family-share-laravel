eefca6d60cb09d7a703e158f18e36bfdeb7b4a52
Oliver Dean Johnston <oliverdeanjohnston@gmail.com>
2025-05-31 23:35:32 +0100

    fix dashboard title

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,8 +1,8 @@
 import { MainLayout } from "@/layouts/main-layout";
 
-export default function Home() {
+export default function Dashboard() {
     return (
-        <MainLayout title="Home" description="Compare Steam library values with friends">
+        <MainLayout title="Dashboard" description="Compare Steam library values with friends">
             <p>test</p>
         </MainLayout>
     );
