8b153426f83f614f7abf0b5ece67980093933395
Oliver Dean Johnston <oliverdeanjohnston@gmail.com>
2025-05-31 12:59:05 +0100

    add dashboard

---
+++ resources/js/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { MainLayout } from "@/layouts/main-layout";
+
+export default function Home() {
+    return (
+        <MainLayout title="Home" description="Compare Steam library values with friends">
+            <p>test</p>
+        </MainLayout>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Settings\PasswordController;
 use App\Http\Controllers\Settings\ProfileController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -13,6 +14,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/password', [PasswordController::class, 'update'])->name('password.update');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
